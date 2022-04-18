6705302b771ea59b3549ef30dbe9b2f1a73bf85b
GmanRiff <103610434+GmanRiff@users.noreply.github.com>
2022-04-18 22:37:52 +0000

    playing with code

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-
 import turtle
 
 s = turtle.getscreen()
-t=turtle.Turtle()
+t = turtle.Turtle()
 
 print("start")
-for x in range (15):
- base_length = 5
- length = base_length + 5
- t.left(base_length)
- t.forward(length) 
+for x in range(72):
+    base_length = 5
+    length = base_length + 5
+    t.left(base_length)
+    t.forward(length)
 t.left(175)
 t.forward(100)
-t.left(200)
+t.left(100)
+t.circle(150)
+t.forward(200)
+t.right(200)
+t.right(225)
+t.forward(100)
+t.right(100)
+t.circle(50)
+t.forward(125)
+t.right(100)
+t.forward(250)
+t.left(50)
+t.circle(75)
+t.forward(300)
+t.left(50)
+t.forward(100)
+t.right(75)
+t.circle(125)
+t.right(50)
+t.forward(100)
+t.circle(200)
+t.right(50)
+t.forward(300)
 t.circle(150)
